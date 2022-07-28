b4cdcecca2ba163fb33628bac85419c4017c84dd
Tadesse Jemal <tadessejemal63@gmail.com>
2022-07-28 17:17:31 +0300

    Add styles on nav

--- js/index.js
+++ js/index.js
@@ -1,21 +1,14 @@
-(() => {
-  const hamburger = document.getElementById('hamburger');
-  const menu = document.getElementById('overlay');
-  let open = false;
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-  const change = () => {
-    if (!open) {
-      hamburger.classList.add('open');
-      menu.classList.add('menu');
-    } else {
-      hamburger.classList.remove('open');
-      menu.classList.remove('menu');
-    }
-    open = !open;
-  };
-
-  hamburger.addEventListener('click', change);
-})();
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
 
 const speakers = [
   {
